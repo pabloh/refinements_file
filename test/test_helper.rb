308ac9892a7ce1a3b39483fdefdfd1d10b8f5211
Pablo Herrero <pablodherrero@gmail.com>
2015-02-04 07:24:14 -0300

    Don't include pry when running the tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,5 @@
 require 'minitest/autorun'
 require 'minitest/reporters' # requires the gem
-require 'pry'
 require 'refinements_file'
 
 Minitest::Reporters.use! [Minitest::Reporters::DefaultReporter.new(color: true)]
